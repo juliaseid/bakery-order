ea4e99ac2fad91ddaa6018a8ca1addae151d91a9
Julia Seidman <juliaseid@gmail.com>
2020-05-08 14:43:27 -0700

    Write out basics of Main and spend time trying to figure out what's wrong w/ variable scoping in Pastry

--- program.cs
+++ program.cs
@@ -7,7 +7,13 @@ namespace Bakery
   {
     public static void Main()
     {
+      Console.WriteLine("Welcome to Carb Overload's online ordering system! How many loaves of bread would you like?");
+      int quantity = int.Parse(Console.ReadLine());
+      Bread breadBuy = new Bread(quantity);
+      breadBuy.Order();
+
       
+
     }
 
   }
