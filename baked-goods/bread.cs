10e02a97ce952dc3d8a9e9389d90ffb4f06beb82
Julia Seidman <juliaseid@gmail.com>
2020-05-08 12:00:19 -0700

    Complete bread pricing logic

--- baked-goods/bread.cs
+++ baked-goods/bread.cs
@@ -31,8 +31,30 @@ namespace Bakery.Items
       }
       else if (over < 2)
       {
-        
+       Console.WriteLine("You are getting " + freeLoaves + " free! If you add one more loaf, you'll get another free.  Would you like more?");
+       if (Console.ReadLine()=="yes") 
+       {
+         Console.WriteLine("Great! We'll add 2 loaves to your order, but you'll only be charged for one!");
+         quantity ++;
+       }
+       else if (Console.ReadLine()=="no")
+       {
+         Console.WriteLine("No problem!");
+       } 
+       else 
+       {
+         Console.WriteLine("Sorry, we didn't understand!  Would you like to pay for one more loaf and get another free?  Please enter 'yes' or 'no'.");
+         if (Console.ReadLine()=="yes")
+         {
+           Console.WriteLine("Great!  We'll add 2 loaves to your order, but you'll only be charged for one!");
+         }
+         else
+         {
+           Console.WriteLine("OK, we'll keep your order as is.");
+         }
+       }
       }
     }
      int total = (quantity-freeLoaves)*price;
+     return total
   }
