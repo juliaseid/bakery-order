050347ed377b7eb69ffac72161edf56d852c8af6
Julia Seidman <juliaseid@gmail.com>
2020-05-08 11:31:08 -0700

    begin implementing bread pricing method

--- baked-goods/bread.cs
+++ baked-goods/bread.cs
@@ -4,12 +4,35 @@ namespace Bakery.Items
 {
   public class Bread
   {
-    public int Price {get; set; }
     public int Quantity {get; set; }
   }
 
   public Bread(int quantity)
   {
-    Price = 5;
     Quantity = quantity;
-  } 
+  }
+
+  private static int price = 5;
+  private int freeLoaves = quantity/3;
+
+  public int Order()
+  {
+    if (quantity % 3 == 0)
+    {
+      Console.WriteLine("You are getting " + freeLoaves + " free!");
+    }
+    else if (quantity % 3 =! 0)
+    {
+      int over = (quantity % 3);
+      if (over >= 2) 
+      {
+        Console.WriteLine("You could get " + freeLoaves + " free loaves of bread.  How many would you like?");
+        quantity += Console.ReadLine();
+      }
+      else if (over < 2)
+      {
+        
+      }
+    }
+     int total = (quantity-freeLoaves)*price;
+  }
